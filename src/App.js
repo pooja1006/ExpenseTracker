9fbda628546c2b821ff56c23d494f21a33e6f09b
pooja1006 <psoni8444@gmail.com>
2023-01-03 23:23:01 +0530

    first commit expense tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from './components/Expenses'
+import NewExpense from './components/NewExpense';
+import React, {useState} from 'react';
+
+const DUMMY_EXPENSES = [
+  {id: 'e1',title: "Car Insurance", amount : 297.86, date: new Date(2021, 2,28)},
+  {id: 'e2',title: "Life Insurance", amount : 8738.86, date: new Date(2020, 6,20)},
+  {id: 'e3',title: "Home Insurance", amount : 4783.86, date: new Date(2021, 7,18)},
+  {id: 'e4',title: "Dog Insurance", amount : 748.86, date: new Date(2020, 5, 8)},
+];
 
 function App() {
+  const [expenses,setExpenses] =useState(DUMMY_EXPENSES)
+  
+  const addExpenseHandler = expense => {
+    setExpenses((prevState)=> { 
+      return [expense,...prevState];
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense = {addExpenseHandler}/>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
